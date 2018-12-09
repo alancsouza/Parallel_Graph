899d137320722a58cadc5a3cd77c1d25fc607ff5
Alan <alancsouza04@gmail.com>
2018-12-09 13:43:38 -0200

    adding code

---
+++ data4_parallel_16.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+data4 = Haberman’s Survival
+
+"""
+
+from graph_parallel_functions import *
+
+data_name = "Habermans Survival"
+result_name = "Result_Data4_16.csv"
+runtime_name = "Runtime_data4_16.csv"
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/haberman/haberman.data'
+data = pd.read_csv(url, sep=',', header=None, skiprows=1)
+
+# setting data precision  
+precisionX = 'float16'
+precisionY = 'int8'
+
+X = data.iloc[:,:-1]
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1)) # Normalizing data between -1 and 1
+X = pd.DataFrame(min_max_scaler.fit_transform(X))
+X = X.astype(precisionX)
+X_type = X.dtypes
+
+y = data.iloc[:,-1].copy() #  Class: (2 for benign, 4 for malignant cancer)
+y[y == 2] = -1
+y = y.astype(precisionY)
+y_type = y.dtypes
+
+print("The {} dataset has {} samples and its bit precision is {} for the X data and {} for y data".format(data_name, X.shape[0], X_type[1], y_type ))
+
+# Computing the Adjacency Matrix:
+Adj_matrix = get_adjacency(X, int_type = precisionY, float_type = precisionX)
+
+# Removing noise in the data
+X_new, y_new = remove_noise(X, y, Adj_matrix, float_type = precisionX)
+
+# Implementing kfold cross validation:
+k = 10 
+
+kf = KFold(n_splits=k, shuffle = True, random_state = 1)
+results = []
+runtime = []
+
+for train_index, test_index in kf.split(X_new):
+    start = time.time()   
+
+    X_train, X_test = X_new.iloc[train_index], X_new.iloc[test_index]
+    y_train, y_test = y_new.iloc[train_index], y_new.iloc[test_index]
+    
+    split_size = round(X_train.shape[0]/50) # 50 for small datasets and 100 for larger data
+    print("The {} data was divided in {} slots \n".format(data_name, split_size))
+
+    if split_size > 1:    
+        # Find the new data set from the support edges 
+        X_train_new, y_train_new = parallel_graph(X_train, y_train, split_size)
+    else:
+        print("Data too small")
+        X_train_new = X_train
+        y_train_new = y_train
+        
+    # Finding the new set of support edges
+    arestas_suporte = support_edges(X_train_new, y_train_new, precisionX, precisionY)
+
+    # Classification:
+    y_hat = classify_data(X_test, y_test, arestas_suporte, int_type = precisionY)
+
+    AUC = compute_AUC(y_test, y_hat)
+    print("The AUC for the {} bit precision is: {}".format(precisionX, AUC))
+
+    end = time.time()
+    final_time = end-start
+    print("The overall model running time is {0:.2f} seconds \n".format(final_time))
+    
+    runtime.append(final_time)
+
+results = pd.DataFrame(results)
+results.to_csv(result_name, sep='\t', encoding='utf-8')
+
+runtime = pd.DataFrame(runtime)
+runtime.to_csv(runtime_name , sep='\t', encoding='utf-8')
---
+++ graph_parallel_functions.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+# Importing LIbraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy.matlib # use repmat function
+from scipy.spatial import distance_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_curve, auc
+from sklearn import preprocessing
+import random
+import time
+import copy
+random.seed(1)
+
+# Computing the adjacency matrix
+def get_adjacency(X, int_type, float_type):
+  dist_matrix = distance_matrix(X,X) # The data type depends on X
+  Adj_matrix = np.zeros(shape = dist_matrix.shape, dtype = int_type)
+  nrow = dist_matrix.shape[0]
+  for i in range(nrow):
+    for j in range(nrow):
+      if (i != j):
+          
+        d1 = (dist_matrix[i,j])/2
+        d1 = d1.astype(float_type)
+        
+        a = pd.DataFrame((X.iloc[i,:]+X.iloc[j,:])/2).T # dtypes are ok
+        d = distance_matrix(a, X) # dtypes are ok
+      
+        d[0,i] = float("inf")
+        d[0,j] = float("inf")
+      
+        compara = (d<d1)
+        if not compara.any():
+          Adj_matrix[i,j] = 1
+          Adj_matrix[j,i] = 1
+          
+  return Adj_matrix
+
+# Removing overlapping samples:
+def remove_noise(X, y, Adj_matrix, float_type):
+  c1 = np.asarray(np.where(y==1)).ravel()
+  c2 = np.asarray(np.where(y==-1)).ravel()
+  A1 = Adj_matrix[:,c1].sum(axis = 0) # sum over columns
+  A2 = Adj_matrix[:,c2].sum(axis = 0)
+  
+  
+  M = pd.DataFrame(Adj_matrix)
+  adj_1 = np.asarray(M.iloc[c1,c1])
+  adj_2 = np.asarray(M.iloc[c2,c2])
+
+  A1h = adj_1.sum(axis = 0)
+  A2h = adj_2.sum(axis = 0)
+  
+  #Computing the quality coefficient Q for each class
+  Q_class1 = A1h / A1
+  Q_class2 = A2h / A2
+
+  # Computing the threshold value t for each class
+  t_class1 = sum(Q_class1) / Q_class1.shape[0]
+  t_class1 = t_class1.astype(float_type)
+  t_class2 = sum(Q_class2) / Q_class2.shape[0]
+  t_class2 = t_class2.astype(float_type)
+  
+  noise_c1 = np.where(Q_class1 < t_class1)
+  noise_c2 = np.where(Q_class2 < t_class2)
+  noise_data = np.c_[noise_c1, noise_c2]
+
+  noise = noise_data.ravel()
+  
+  # Filtering the data
+  X_new = X.drop(noise)
+  y_new = y.drop(noise)
+  
+  print("{} samples where removed from the data. \n".format(X.shape[0]-X_new.shape[0]))
+  
+  return X_new, y_new
+
+# Finding the separation border:
+def get_borda(y, Adj_matrix, int_type):
+  y_t = pd.DataFrame(y).T
+  
+  ncol = y_t.shape[1]
+  mask = np.matlib.repmat(y_t, ncol, 1)
+  mask2 = pd.DataFrame(mask*Adj_matrix)  
+  borda = pd.DataFrame(np.zeros(ncol, dtype = int_type)).T
+
+  for idx in range(ncol):
+    a1 =  sum(-y_t.iloc[0, idx] == mask2.iloc[idx,:]) # check if the labels are different
+    if a1 > 0:
+      borda[idx] = 1
+    
+  return borda
+
+# Finding the support edges:
+def get_arestas_suporte(X, y, borda, Adj_matrix, int_type):
+  
+  X = np.asarray(X)
+  y_t = pd.DataFrame(y).T
+  ncol = y_t.shape[1]
+  mask = np.matlib.repmat(y_t, ncol, 1)
+  nrow = Adj_matrix.shape[0]
+  maskBorda = np.matlib.repmat(borda == 1, nrow, 1)
+  maskBorda = np.asarray(maskBorda, int_type)
+
+  # Filtrando apenas as linhas que não correspondem à margem:
+  aux = maskBorda * np.transpose(maskBorda)
+
+  # Removendo arestas que não pertencem ao grafo:
+  aux = Adj_matrix * aux
+
+  # Discartando arestas entre vértices que pertencem à mesma classe:
+  aux1 = aux + (mask * aux)
+  aux2 = aux - (mask * aux)
+  aux1 = np.asarray(aux1)
+  aux2 = np.asarray(aux2)
+  aux = aux1 * np.transpose(aux2)
+
+  # transformando a matriz em binária:
+  aux  = (aux != 0)
+
+  arestas = np.where(aux == 1)
+
+  arestas = np.transpose(np.asarray(arestas, int_type))
+  nrow_arestas = arestas.shape[0]
+  ncol_arestas = arestas.shape[1]
+
+  arestas_suporte = []
+  y_suporte = []
+
+  y_arr = np.asarray(y, int_type)
+
+  for i in range(nrow_arestas):
+    for j in range(ncol_arestas):
+    
+      idx = arestas[i,j]
+      arestas_suporte.append(X[idx,:])
+      y_suporte.append(y_arr[idx])
+
+  
+  
+  X_suporte = np.asarray(arestas_suporte)
+  y_suporte = np.asarray(y_suporte)
+  
+  return X_suporte, y_suporte
+
+# Another support edges function that contains the other functions
+def support_edges(X_train, y_train, float_type, int_type):
+  
+  Adj_matrix = get_adjacency(X_train, int_type, float_type)
+  
+  borda = get_borda(y_train, Adj_matrix, int_type = precisionY)
+  X_suporte, y_suporte = get_arestas_suporte(X_train, y_train, borda, Adj_matrix, int_type)
+  
+  arestas_suporte = np.c_[X_suporte, y_suporte]
+  if arestas_suporte.shape[0] > 0:
+    arestas_suporte = np.unique(arestas_suporte, axis = 0)
+  
+  return arestas_suporte
+
+# Classification
+def classify_data(X_test, y_test, arestas_suporte, int_type):
+  
+  X_suporte = arestas_suporte[:,:-1]
+  y_suporte = arestas_suporte[:,-1]
+  nrow = X_test.shape[0]
+  dist_test = distance_matrix(X_test, X_suporte) # compute the distance from the sample to the support egdes
+
+  y_hat = np.zeros(nrow, int_type)
+
+  for idx in range(nrow):
+    dist = dist_test[idx,:]
+    min_idx = np.argmin(dist)
+    y_hat[idx] = arestas_suporte[min_idx, -1] 
+  
+  return y_hat
+
+# Performance measure using AUC
+def compute_AUC(y_test, y_hat):
+  fpr, tpr, _ = roc_curve(y_test, y_hat)
+  roc_auc = auc(fpr, tpr)
+  
+  return roc_auc
+
+# Parallel graph method:
+def parallel_graph(X_train, y_train, split_size, float_precision = precisionX, int_precision = precisionY):
+  data_train = np.c_[X_train, y_train]
+  np.random.shuffle(data_train)
+
+  data_split = np.array_split(data_train, split_size)
+  arestas_suporte_final = []
+  
+  for i in range(split_size):
+    data = pd.DataFrame(data_split[i])
+    X_train = data.iloc[:,:-1]
+    y_train = data.iloc[:, -1]
+
+    # Finding the support edges from this slot of data:
+    arestas_suporte = support_edges(X_train, y_train, precisionX, precisionY)
+
+    arestas_suporte_final.append(arestas_suporte)
+    
+  arr = arestas_suporte_final[0]
+
+  for i in range(split_size-1):
+    i = i+1
+    arr = np.concatenate((arr, arestas_suporte_final[i]), axis = 0)  
+    
+  data_train_new = pd.DataFrame(arr)
+  X_train_new = data_train_new.iloc[:,:-1]
+  y_train_new = data_train_new.iloc[:,-1]
+  
+  return X_train_new, y_train_new
